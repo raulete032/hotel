754441aaefeafa3cbc25522d80ea65b8f21fcfed
raul <rcr_032@hotmail.com>
2023-05-20 14:24:27 +0200

    contenedores de al pulsar sobre enlaces

--- js/functions.js
+++ js/functions.js
@@ -10,7 +10,9 @@ for(let i=0;i<enlacesCabecera.length;i++)
 
 
 
-
+/**
+ * Función que añade la class active al enlace que se pulsa
+ */
 function cambiarActiveEnlace(){
 
     let enlace= this;
@@ -25,8 +27,32 @@ function cambiarActiveEnlace(){
         }
     }
     enlace.classList.add("active");
+    muestraSeccion(this);
 }
 
 
+function muestraSeccion(enlace){
+
+    let id= enlace.id;
+
+    let contenedores= document.querySelectorAll("div[data-referenciaEnlace]");
+
+    for(let i=0;i<contenedores.length;i++){
+        contenedores[i].style.display="none";
+    }
+
+    let sentencia= "div[data-referenciaEnlace="+id+"]";
+    switch(id){
+        case "enlaceReservas": document.querySelector(sentencia).style.display="block";
+        break;
+        case "enlaceHabitaciones": document.querySelector(sentencia).style.display="block";
+        break;
+        case "enlaceEmpleados": document.querySelector(sentencia).style.display="block";
+        break;
+        case "enlaceRestaurente": document.querySelector(sentencia).style.display="block";
+        break;
+    }
 
 
+
+}
